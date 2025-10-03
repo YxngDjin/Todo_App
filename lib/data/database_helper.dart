d3da25eaa562471d1632c0da46b5ac45ac87565b
Dennis Lehmann <dennisl2002@gmx.de>
2025-10-03 16:45:54 +0200

    Add SQFLite Database for Saving Tasks

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,67 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:eatak/models/todo_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._internal();
+  static Database? _database;
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'todo.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE todos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT,
+            isDone INTEGER,
+            subtasks TEXT,
+            category INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertTodo(Todo todo) async {
+    final db = await database;
+    return await db.insert(
+      'todos',
+      todo.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Todo>> getTodos() async {
+    final db = await database;
+    final result = await db.query('todos');
+    return result.map((map) => Todo.fromMap(map)).toList();
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    final db = await database;
+    return await db.update(
+      'todos',
+      todo.toMap(),
+      where: 'id = ?',
+      whereArgs: [todo.id],
+    );
+  }
+
+  Future<int> deleteTodo(int id) async {
+    final db = await database;
+    return await db.delete('todos', where: 'id = ?', whereArgs: [id]);
+  }
+}
--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 enum Category { health, mentalHealth, work, others }
 
 class Todo {
@@ -14,12 +16,42 @@ class Todo {
     this.subtasks = const [],
     this.category = Category.others,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'task': task,
+      'isDone': isDone ? 1 : 0,
+      'subtasks': jsonEncode(subtasks.map((s) => s.toMap()).toList()),
+      'category': category.index,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'],
+      task: map['task'],
+      isDone: map['isDone'] == 1,
+      subtasks: (jsonDecode(map['subtasks']) as List)
+          .map((s) => Subtask.fromMap(s))
+          .toList(),
+      category: Category.values[map['category']],
+    );
+  }
 }
 
 class Subtask {
   String title;
   bool isDone;
   Subtask({required this.title, this.isDone = false});
+
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'isDone': isDone ? 1 : 0};
+  }
+
+  factory Subtask.fromMap(Map<String, dynamic> map) {
+    return Subtask(title: map['title'], isDone: map['isDone'] == 1);
+  }
 }
 
 extension CategoryExtension on Category {
